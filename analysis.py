70ad9889bc0957d6a24bd0562f2c2c224d9a8568
nicholash711 <nicholash489@gmail.com>
2023-11-15 20:25:22 -0600

    separating wrong predictions

---
+++ analysis.py
@@ -0,0 +1,24 @@
+import os
+import json
+
+analysis_dir = "./nli_test_analysis/"
+
+def main():
+    f = open("./output/eval_predictions.jsonl", )
+    json_list = list(f)
+    
+    f.close()
+    
+    f = open(analysis_dir + "wrong_predictions.jsonl", "w")
+    
+    for entry in json_list:
+        js = json.loads(entry)
+        if js["label"] != js["predicted_label"]:
+            f.write(entry);
+            
+    f.close()
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ eval.py
@@ -0,0 +1,15 @@
+# Perform evaluation on data, hopefully explore corpus and LM
+import os
+import json
+import datasets
+from helpers import prepare_dataset_nli, compute_accuracy
+from transformers import Trainer
+
+
+def main():
+    print("Hello World!")
+    pass
+
+
+if __name__ == "__main__":
+    main()
